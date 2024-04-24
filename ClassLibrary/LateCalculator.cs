5ecec7f66b8819f1bb5c4ce5a8c103de8c4badd9
228300 - Trần Lê Hiếu Trung <trungtran2k01@gmail.com>
2024-04-24 12:51:11 +0700

    challenge and unit test

---
+++ ClassLibrary/LateCalculator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace ClassLibrary
+{
+    public class LateCalculator
+    {
+
+        public static TimeSpan CalculateLate(DateTime shiftStart, DateTime shiftEnd, DateTime breakStart, DateTime breakEnd, DateTime signIn, DateTime? leaveFrom = null, DateTime? leaveTo = null)
+        {
+
+            TimeSpan late = TimeSpan.Zero;
+            TimeSpan breakPeriod = breakEnd - breakStart;
+            TimeSpan shift = shiftEnd - shiftStart - breakPeriod;
+            //check if the signIn time is after the shift end time
+            if (signIn >= shiftEnd)
+            {
+                late = shift + (signIn - shiftEnd);
+            }
+            //check if signIn in shift
+            if (signIn > shiftStart && signIn < shiftEnd)
+            {
+                if (signIn < breakStart)
+                {
+                    late = signIn - shiftStart;
+                }
+                else if (signIn >= breakStart && signIn <= breakEnd)
+                {
+                    late = breakStart - shiftStart;
+                }
+                else if (signIn > breakEnd)
+                {
+                    //check if the signIn is after the breakEnd 
+                    TimeSpan shift1 = breakStart - shiftStart;
+                    late = signIn - breakEnd + shift1;
+                }
+                if (leaveFrom.HasValue && leaveTo.HasValue)
+                {
+
+                    TimeSpan leavePeriod = (TimeSpan)(leaveTo - leaveFrom);
+              
+                    if (signIn >= leaveFrom && signIn <= leaveTo)
+                    {
+                        late = (TimeSpan)(leaveFrom - shiftStart);
+                    }
+                    else if (signIn > leaveFrom)
+                    {
+                        //check if there is leave time overlapping with the sign in time or break end time
+                        if (leaveFrom <= breakStart && leaveTo >= breakEnd)
+                        {
+                            late -= leavePeriod - breakPeriod;
+                        }
+                        else
+                        {
+                            late -= leavePeriod;
+                        }
+                    }
+                }
+
+            }
+
+            return late;
+        }
+    }
+}
+
+
---
+++ NUnitTestCodingChallenge/UnitTestLateCalculator.cs
@@ -0,0 +1,149 @@
+﻿using ClassLibrary;
+
+namespace NUnitTestCodingChallenge;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+
+    [Test]
+    public void TestCalculateLate_SignInAfterShiftEnd()
+    {
+        DateTime shiftStart = new DateTime(2024, 4, 24, 8, 0, 0);
+        DateTime shiftEnd = new DateTime(2024, 4, 24, 17, 0, 0);
+        DateTime breakStart = new DateTime(2024, 4, 24, 12, 0, 0);
+        DateTime breakEnd = new DateTime(2024, 4, 24, 13, 0, 0);
+        DateTime signIn = new DateTime(2024, 4, 24, 17, 15, 0);
+        DateTime? leaveFrom = null;
+        DateTime? leaveTo = null;
+
+        TimeSpan expectedLate = TimeSpan.FromHours(8) + TimeSpan.FromMinutes(15);
+        TimeSpan actualLate = LateCalculator.CalculateLate(shiftStart, shiftEnd, breakStart, breakEnd, signIn, leaveFrom, leaveTo);
+
+        Assert.AreEqual(expectedLate, actualLate);
+    }
+
+    [Test]
+    public void TestCalculateLate_SignInBeforeBreak()
+    {
+        DateTime shiftStart = new DateTime(2024, 4, 24, 8, 0, 0);
+        DateTime shiftEnd = new DateTime(2024, 4, 24, 17, 0, 0);
+        DateTime breakStart = new DateTime(2024, 4, 24, 12, 0, 0);
+        DateTime breakEnd = new DateTime(2024, 4, 24, 13, 0, 0);
+        DateTime signIn = new DateTime(2024, 4, 24, 8, 30, 0);
+        DateTime? leaveFrom = null;
+        DateTime? leaveTo = null;
+
+        TimeSpan expectedLate = TimeSpan.FromMinutes(30);
+        TimeSpan actualLate = LateCalculator.CalculateLate(shiftStart, shiftEnd, breakStart, breakEnd, signIn, leaveFrom, leaveTo);
+
+        Assert.AreEqual(expectedLate, actualLate);
+    }
+
+    [Test]
+    public void TestCalculateLate_SignInDuringBreak()
+    {
+        DateTime shiftStart = new DateTime(2024, 4, 24, 8, 0, 0);
+        DateTime shiftEnd = new DateTime(2024, 4, 24, 17, 0, 0);
+        DateTime breakStart = new DateTime(2024, 4, 24, 12, 0, 0);
+        DateTime breakEnd = new DateTime(2024, 4, 24, 13, 0, 0);
+        DateTime signIn = new DateTime(2024, 4, 24, 12, 30, 0);
+        DateTime? leaveFrom = null;
+        DateTime? leaveTo = null;
+
+        TimeSpan expectedLate = TimeSpan.FromHours(4);
+        TimeSpan actualLate = LateCalculator.CalculateLate(shiftStart, shiftEnd, breakStart, breakEnd, signIn, leaveFrom, leaveTo);
+
+        Assert.AreEqual(expectedLate, actualLate);
+    }
+
+    [Test]
+    public void TestCalculateLate_SignInAfterBreak()
+    {
+        DateTime shiftStart = new DateTime(2024, 4, 24, 8, 0, 0);
+        DateTime shiftEnd = new DateTime(2024, 4, 24, 17, 0, 0);
+        DateTime breakStart = new DateTime(2024, 4, 24, 12, 0, 0);
+        DateTime breakEnd = new DateTime(2024, 4, 24, 13, 0, 0);
+        DateTime signIn = new DateTime(2024, 4, 24, 14, 30, 0);
+        DateTime? leaveFrom = null;
+        DateTime? leaveTo = null;
+
+        TimeSpan expectedLate = TimeSpan.FromHours(5) + TimeSpan.FromMinutes(30);
+        TimeSpan actualLate = LateCalculator.CalculateLate(shiftStart, shiftEnd, breakStart, breakEnd, signIn, leaveFrom, leaveTo);
+
+        Assert.AreEqual(expectedLate, actualLate);
+    }
+
+    [Test]
+    public void TestCalculateLate_SignInDuringLeave()
+    {
+        DateTime shiftStart = new DateTime(2024, 4, 24, 8, 0, 0);
+        DateTime shiftEnd = new DateTime(2024, 4, 24, 17, 0, 0);
+        DateTime breakStart = new DateTime(2024, 4, 24, 12, 0, 0);
+        DateTime breakEnd = new DateTime(2024, 4, 24, 13, 0, 0);
+        DateTime signIn = new DateTime(2024, 4, 24, 9, 0, 0);
+        DateTime leaveFrom = new DateTime(2024, 4, 24, 8, 0, 0);
+        DateTime leaveTo = new DateTime(2024, 4, 24, 10, 0, 0);
+
+        TimeSpan expectedLate = TimeSpan.Zero;
+        TimeSpan actualLate = LateCalculator.CalculateLate(shiftStart, shiftEnd, breakStart, breakEnd, signIn, leaveFrom, leaveTo);
+
+        Assert.AreEqual(expectedLate, actualLate);
+    }
+
+    [Test]
+    public void TestCalculateLate_SignInDuringAnyLeave()
+    {
+        DateTime shiftStart = new DateTime(2024, 4, 24, 8, 0, 0);
+        DateTime shiftEnd = new DateTime(2024, 4, 24, 17, 0, 0);
+        DateTime breakStart = new DateTime(2024, 4, 24, 12, 0, 0);
+        DateTime breakEnd = new DateTime(2024, 4, 24, 13, 0, 0);
+        DateTime signIn = new DateTime(2024, 4, 24, 9, 30, 0);
+        DateTime leaveFrom = new DateTime(2024, 4, 24, 9, 0, 0);
+        DateTime leaveTo = new DateTime(2024, 4, 24, 11, 0, 0);
+
+        TimeSpan expectedLate = TimeSpan.FromHours(1);
+        TimeSpan actualLate = LateCalculator.CalculateLate(shiftStart, shiftEnd, breakStart, breakEnd, signIn, leaveFrom, leaveTo);
+
+        Assert.AreEqual(expectedLate, actualLate);
+    }
+
+    [Test]
+    public void TestCalculateLate_SignInBeforeLeave()
+    {
+        DateTime shiftStart = new DateTime(2024, 4, 24, 8, 0, 0);
+        DateTime shiftEnd = new DateTime(2024, 4, 24, 17, 0, 0);
+        DateTime breakStart = new DateTime(2024, 4, 24, 12, 0, 0);
+        DateTime breakEnd = new DateTime(2024, 4, 24, 13, 0, 0);
+        DateTime signIn = new DateTime(2024, 4, 24, 8, 30, 0);
+        DateTime leaveFrom = new DateTime(2024, 4, 24, 9, 0, 0);
+        DateTime leaveTo = new DateTime(2024, 4, 24, 11, 0, 0);
+
+        TimeSpan expectedLate = TimeSpan.FromMinutes(30);
+        TimeSpan actualLate = LateCalculator.CalculateLate(shiftStart, shiftEnd, breakStart, breakEnd, signIn, leaveFrom, leaveTo);
+
+        Assert.AreEqual(expectedLate, actualLate);
+    }
+
+    [Test]
+    public void TestCalculateLate_SignInAfterLeave()
+    {
+        DateTime shiftStart = new DateTime(2024, 4, 24, 8, 0, 0);
+        DateTime shiftEnd = new DateTime(2024, 4, 24, 17, 0, 0);
+        DateTime breakStart = new DateTime(2024, 4, 24, 12, 0, 0);
+        DateTime breakEnd = new DateTime(2024, 4, 24, 13, 0, 0);
+        DateTime signIn = new DateTime(2024, 4, 24, 14, 30, 0);
+        DateTime leaveFrom = new DateTime(2024, 4, 24, 9, 0, 0);
+        DateTime leaveTo = new DateTime(2024, 4, 24, 11, 0, 0);
+
+        TimeSpan expectedLate = TimeSpan.FromMinutes(320);
+        TimeSpan actualLate = LateCalculator.CalculateLate(shiftStart, shiftEnd, breakStart, breakEnd, signIn, leaveFrom, leaveTo);
+
+        Assert.AreEqual(expectedLate, actualLate);
+    }
+
+}
